df8fa7125b2920a54d2c768d5ddb889b25e3bab8
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-03 15:00:04 +0530

    Update favicon path and change document title; add social media links constants

---
+++ constants.js
@@ -0,0 +1,7 @@
+export const links = {
+	"GitHub": "https://github.com/jessicarandoll",
+	"LinkedIn": "https://www.linkedin.com/in/jessicarandoll/",
+	"Twitter": "https://twitter.com/jessicarandoll",
+	"Instagram": "https://www.instagram.com/jessicarandoll/",
+	"Email": "mailto:lG0Q7@jessicarandoll.com"
+}
